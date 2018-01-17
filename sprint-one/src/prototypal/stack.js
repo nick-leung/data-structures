f9cb2667e26358dad3c38f91d88a9c33b9adbded
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-17 13:09:26 -0800

    Completed prototypal pattern for stack.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.count = 0;
+  newStack.storage = {};
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    (this.storage)[this.count] = value;
+    this.count++;
+  },
+  pop: function(value) {
+    if (this.count > 0) {
+      this.count--;
+      var popped = (this.storage)[this.count];
+      delete (this.storage)[this.count];
+      return popped;
+    }
+  },
+  size: function() {
+    return this.count;
+  },
+};
 
 
