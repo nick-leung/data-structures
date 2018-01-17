436d587e322f9d94b836e98d58167ec390c4d64e
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-17 14:49:24 -0800

    Completed es6 pattern for queue.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
+    this.first = 0;
+    this.last = 0;
   }
-
+  enqueue(value) {
+    this.count++;
+    this.last++;
+    (this.storage)[this.last] = value;
+  }
+  dequeue() {
+    if (this.count > 0) {
+      this.count--;
+      this.first++;
+      var popped = (this.storage)[this.first];
+      delete (this.storage)[this.first];
+      return popped;
+    }
+  }
+  size() {
+    return this.count;
+  }
 }
