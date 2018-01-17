3afa0f05a1977dd2194d927473fe7cea32540fdb
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-17 13:12:24 -0800

    Completed prototypal pattern for queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.count = 0;
+  newQueue.first = 0;
+  newQueue.last = 0;
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
-
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.last++;
+    (this.storage)[this.last] = value;
+  },
+  dequeue: function(value) {
+    if (this.count > 0) {
+      this.count--;
+      this.first++;
+      var popped = (this.storage)[this.first];
+      delete (this.storage)[this.first];
+      return popped;
+    }
+  },
+  size: function() {
+    return this.count;
+  },
+};
