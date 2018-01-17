8b448318fb027ed17fe63c139b2a7c2ae4a7f510
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-17 14:41:36 -0800

    Completed es6 pattern for stack.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+  push(value) {
+    (this.storage)[this.count] = value;
+    this.count++;
+  }
+  pop() {
+    if (this.count > 0) {
+      this.count--;
+      var popped = (this.storage)[this.count];
+      delete (this.storage)[this.count];
+    }
+    return popped;
+  }
+  size() {
+    return this.count;
   }
 
 }
