dede91681710d2f07e31d51a6abf9a50aa2a2eb6
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-17 12:52:30 -0800

    Removed console.log statements.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.count = 0;
+  newStack.storage = {};
+  extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    (this.storage)[this.count] = value;
+  },
+  pop: function(value) {
+    if (this.count > 0) {
+      var valuePopped = (this.storage)[this.count];
+      delete (this.storage)[this.count];
+      this.count--;
 
+      return valuePopped;
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
